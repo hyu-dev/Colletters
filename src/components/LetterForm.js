c9fa76a86aa1c164ecf54f5a025f943229f62cdb
hyu-dev <hyu630115@gmail.com>
2021-05-28 17:22:25 +0900

    Update LetterForm.js

--- src/components/LetterForm.js
+++ src/components/LetterForm.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
 
 import { FaSlackHash, FaTrash, FaPlus } from 'react-icons/fa';
 
@@ -7,16 +7,34 @@ import { OpenTagProvider } from '../data/ModalContext';
 import '../scss/LetterForm.scss';
 import UserTag from './UserTag';
 import { withRouter } from 'react-router';
+import { IconContext } from 'react-icons';
 
 const fileInput = styled.input`
-    
+    display: none;
 `;
 
 const ImageContainer = styled.div`
-
+    width: 150px;
+    height: 150px;
+    background: #F6F6F6;
+    ${ props =>
+        props.onChange &&
+        css``
+    }
+    ${ props => 
+        props.createDiv &&
+        css`
+            &::before {
+                content: '추가 사진';
+            }
+        `
+    }
 `;
 
 function LetterForm(props) {
+    const [tagColor, setTagColor] = useState('#dee2e6')
+    const [trashColor, setTrashColor] = useState('#dee2e6')
+
     return (
         <>
         <OpenTagProvider>
@@ -31,23 +49,44 @@ function LetterForm(props) {
 
                     </div>
                 </div>
-                <div className="tagContainer">
-                    <div>
-                        <FaSlackHash />
-                        <input type="text" />
+                <aside className="tagContainer">
+                    <div className="inputTagContainer">
+                        <div>
+                            <IconContext.Provider value={ { size: 35, color: tagColor } }>
+                                <FaSlackHash />
+                            </IconContext.Provider>
+                        </div>
+                        <input type="text" placeholder="꼬리표 등록"/>
                     </div>
                     <ul>
                         <li>
-                            <FaSlackHash />
+                            <IconContext.Provider value={ { size: 25, color: '#87E8D6' } }>
+                                <FaSlackHash />
+                            </IconContext.Provider>
                             <p>아무거나 막 적어</p>
-                            <FaTrash />
+                            <div>
+                                <IconContext.Provider value={ { size: 20, color: trashColor } }>
+                                    <FaTrash/>
+                                </IconContext.Provider>
+                            </div>
+                        </li>
+                        <li>
+                            <IconContext.Provider value={ { size: 25, color: '#87E8D6' } }>
+                                <FaSlackHash />
+                            </IconContext.Provider>
+                            <p>아무거나 막 적어</p>
+                            <div>
+                                <IconContext.Provider value={ { size: 20, color: trashColor } }>
+                                    <FaTrash/>
+                                </IconContext.Provider>
+                            </div>
                         </li>
                     </ul>
                     <select>
                         <option>모두 보다</option>
                         <option>나만 보다</option>
                     </select>
-                </div>
+                </aside>
                 <div className="buttonContainer">
                     <button className="writeBtn">끼적이다</button>
                     <button className="backBtn" onClick={ () => { props.history.push({ pathname: '/main' }) } }>나가다</button>
